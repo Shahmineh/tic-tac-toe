5599aa69d803142c8c1ac501a9a1fc6928868144
Shamin Krook <shahmineh.gh@hotmail.com>
2019-01-16 11:36:46 +0100

    Get the next player

--- src/index.js
+++ src/index.js
@@ -2,24 +2,16 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-
-class Square extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: null,
-  //   };
-  // }
-  render() {
-    return(
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+// the Square components are now controlled components. The Board has full control over them.
+function Square(props) {
+  return(
+    <button 
+      className="square" 
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -27,13 +19,20 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    if ( calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ 
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+     });
   }
 
   renderSquare(i) {
